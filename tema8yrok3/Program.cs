2df50146316e111611589d21bf55e60e10a90768
Clawiatura <maksimov19992805@icloud.com>
2024-01-30 11:40:31 +0200

    tema 8 yrok 3

---
+++ tema8yrok3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+//Console.WriteLine("Введите первое число: ");
+//int first = int.Parse(Console.ReadLine());
+
+
+//Console.WriteLine("Введите второе число: ");
+//int second = int.Parse(Console.ReadLine());
+
+
+//Console.WriteLine("Введите третье число: ");
+//int third = int.Parse(Console.ReadLine());
+
+//int summa = first + second + third;
+//int multi = first * second * third;
+
+//Console.WriteLine($"Сумма {summa} Умножение {multi}");
+
+Console.WriteLine("Введите целое число: ");
+int first = int.Parse(Console.ReadLine());
+int res = first % 8;
+Console.WriteLine(res);
+
+
+
+
