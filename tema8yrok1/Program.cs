efde40d9160f38ad83141199d429150de258a138
Clawiatura <maksimov19992805@icloud.com>
2024-01-30 11:32:32 +0200

    basket

--- tema8yrok1/Program.cs
+++ tema8yrok1/Program.cs
@@ -1,8 +1,8 @@
-﻿//int Petya = 10;
-//int Alewa = Petya - 4;
-//int Vanya = Alewa + 2;
-//int all = Petya + Vanya + Alewa;
-//Console.WriteLine($"Петя взял {Petya} книг, Алеша взял {Alewa} книг, а Ваня взял {Vanya} книг. Вместе взяли {all} книги. ");
+﻿int Petya = 10;
+int Alewa = Petya - 4;
+int Vanya = Alewa + 2;
+int all = Petya + Vanya + Alewa;
+Console.WriteLine($"Петя взял {Petya} книг, Алеша взял {Alewa} книг, а Ваня взял {Vanya} книг. Вместе взяли {all} книги. ");
 
 int basket = 23;
 int basket2 = basket - 11;
