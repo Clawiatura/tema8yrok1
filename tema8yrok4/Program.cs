fbd4c6b99ff209c7c33db03eafbb647d67727568
Clawiatura <maksimov19992805@icloud.com>
2024-01-30 11:48:56 +0200

    tema8yrok4

---
+++ tema8yrok4/Program.cs
@@ -0,0 +1,13 @@
+﻿int mather = 13;
+int ate = 9;
+int res = mather - ate;
+Console.WriteLine($"Съели {res} ");
+
+int widthMagazine = 25;
+int widthNewspaper = widthMagazine + 16;
+Console.WriteLine( $"Ширина газеты {widthNewspaper} ");
+
+int potato = 44;
+int beet = potato / 2;
+int onion =beet / 2;
+Console.WriteLine(onion);
