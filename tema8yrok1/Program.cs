0f052cd0e5425207d54f961746fbc22a403a77ae
Clawiatura <maksimov19992805@icloud.com>
2024-01-30 11:28:22 +0200

    Добавьте файлы проекта.

---
+++ tema8yrok1/Program.cs
@@ -0,0 +1,5 @@
+﻿int Petya = 10;
+int Alewa = Petya - 4;
+int Vanya = Alewa + 2;
+int all = Petya + Vanya + Alewa;
+Console.WriteLine($"Петя взял {Petya} книг, Алеша взял {Alewa} книг, а Ваня взял {Vanya} книг. Вместе взяли {all} книги. ");
