fc0437c17eb9830263dd9df95638fa678238be48
Clawiatura <maksimov19992805@icloud.com>
2024-02-19 12:37:42 +0200

    tema8yrok2

---
+++ tema8yr2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//int ate = 9;
+//int buyMather = 13;
+//buyMather -= ate;
+//Console.WriteLine(buyMather);
+
+//int magazine = 25;
+//int less = 16;
+//magazine -= less;
+//Console.WriteLine($"Ширина газеты = {magazine}");
+
+int potato = 44;
+int beetless =  4;
+int onionLess = 2;
+potato/=beetless;
+potato /= onionLess; ;
+Console.WriteLine($"{potato}");
+
+
+
+
+
