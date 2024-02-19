f8832a966ca64fcbc17c74afd3c67ba81aa38bdd
Clawiatura <maksimov19992805@icloud.com>
2024-02-19 14:52:16 +0200

    while dowhile

---
+++ while do while/Program.cs
@@ -0,0 +1,13 @@
+﻿Random random = new Random();
+int a = random.Next(1, 6);
+int i = 3;
+while (i > 0)
+{
+    Console.WriteLine("Введите число : ");
+    int b = int.Parse(Console.ReadLine());
+    if (b == a) Console.WriteLine("Вы победили!"); break;
+} i--;
+{
+    if (i > 0) Console.WriteLine($"ВЫ не угадали осталось {i} попыток");
+    else Console.WriteLine($"Ты проиграл, число {a}");
+} 
---
+++ while dowhile/Program.cs
@@ -0,0 +1,53 @@
+﻿//Console.WriteLine("Число от 1 до 5, попробуй угадать.");
+//Random random = new Random();
+//int a = random.Next(1, 6);
+//int i = 3;
+
+//while (i > 0)
+//{
+//        Console.Write("Введите свой вариант: ");
+//        int b = int.Parse(Console.ReadLine());
+//        if (b == a)
+//    {
+//        Console.WriteLine("Ты угадал число");
+//        break;
+//    }
+//    else
+//    {
+//        i--;
+//        if (i > 0)
+//        {
+//            Console.WriteLine($"Неправильно! У тебя осталось {i} попыток.");
+//        }
+//        else
+//        {
+//            Console.WriteLine($"число {a}. Попробуй еще раз");
+//        }
+//    }
+//}
+
+
+//Random random = new Random();
+//int i = 0;
+//while (i != 7)
+//{
+//    int a = random.Next(0, 11);
+//    Console.WriteLine(a);
+//    i++;
+//}
+int sum = 0;
+int a = 0;
+int i = 1;
+while (i > 0)
+{
+    Console.WriteLine("Введите число: ");
+    a = int.Parse(Console.ReadLine());
+    sum += a;
+    i++;
+    
+    if (a == 0)
+    {
+        
+        Console.WriteLine($"Сумма указаных чисел = {sum}"); break;
+    }
+}
