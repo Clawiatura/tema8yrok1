7dc2771e101c36bc741ea46ce1e6002523cd9b15
Clawiatura <maksimov19992805@icloud.com>
2024-02-27 13:48:27 +0200

    mas2

--- mas1/Program.cs
+++ mas1/Program.cs
@@ -1,23 +1,46 @@
-﻿string[] mas = new string[12];
-mas[0] = "освоении";
-mas[1] = "остальное";
-mas[2] = "по плечу";
-mas[3] = "сложный";
-mas[4] = "но если";
-mas[5] = "програмирование";
-mas[6] = "многие бросают";
-mas[7] = "остальное будет";
-mas[8] = "освоить их";
-mas[9] =  "этап в";
-mas[10] = " на этом этапе";
-mas[11] = "Массивы - ";
-for(int i = 0; i < mas.Length; i++)
+﻿//string[] mas = new string[12];
+//mas[0] = "освоении";
+//mas[1] = "остальное";
+//mas[2] = "по плечу";
+//mas[3] = "сложный";
+//mas[4] = "но если";
+//mas[5] = "програмирование";
+//mas[6] = "многие бросают";
+//mas[7] = "остальное будет";
+//mas[8] = "освоить их";
+//mas[9] =  "этап в";
+//mas[10] = " на этом этапе";
+//mas[11] = "Массивы - ";
+//for(int i = 0; i < mas.Length; i++)
+//{
+//    Console.Write(mas[i]+" ");
+//}
+//Console.WriteLine();
+//string[] mas2 = {"Массивы - ", "сложный", " этап в", "програмирование", "многие бросают", "но если освоить их", "остальное будет", "по плечу" };
+//for(int i = 0;i < mas2.Length; i++)
+//{
+//    Console.Write(mas2[i] + " ");
+//}
+
+
+int[] mas = new int[10];
+Random random = new Random();
+int a = 0;
+for (int i = 0; i < mas.Length; i++)
 {
-    Console.Write(mas[i]+" ");
+    mas[i] = random.Next(0, 20);
+    Console.Write(mas[i] + " ");
 }
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i]%2 == 0)
+        {
+            a++;
+        }
+
+    }
+    
+
+
 Console.WriteLine();
-string[] mas2 = {"Массивы - ", "сложный", " этап в", "програмирование", "многие бросают", "но если освоить их", "остальное будет", "по плечу" };
-for(int i = 0;i < mas2.Length; i++)
-{
-    Console.Write(mas2[i] + " ");
-}
+Console.WriteLine(a);
