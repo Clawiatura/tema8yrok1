e6a9cfbd0ebf99851338fb71196121cb09d788df
Clawiatura <maksimov19992805@icloud.com>
2024-03-06 19:08:03 +0200

    Двухмерные массивы

---
+++ Dvumernuemassivu/Program.cs
@@ -0,0 +1,66 @@
+﻿//using System.Linq.Expressions;
+
+//double[,] mas = new double[3, 3];
+//double[,] mas1 = new double[3, 3];
+
+//Random random = new Random();
+//for (int i = 0; i <= mas.GetUpperBound(0); i++)
+//{
+//    for (int j = 0; j <= mas.GetUpperBound(1); j++)
+//    {
+//        mas[i, j] = random.NextDouble() * 20;
+//        Console.Write($"{mas[i, j]:F2} ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//        Console.Write("Хотите заменить число? Yes/No ");
+//        string answer = Console.ReadLine();
+//if (answer == "Yes")
+//{
+//    Console.Write("Введите номер строки: ");
+//    int n = int.Parse(Console.ReadLine());
+//    Console.Write("Введите номер столбца: ");
+//    int m = int.Parse(Console.ReadLine());
+//    Console.Write("Введите новое значение: ");
+//    int number = int.Parse(Console.ReadLine());
+//    mas[n, m] = number;
+//    for (int i = 0; i <= mas.GetUpperBound(0); i++)
+//    {
+//        for (int j = 0; j <= mas.GetUpperBound(1); j++)
+//        {
+
+//            Console.Write($"{mas[i, j]:F2} ");
+//        }
+//        Console.WriteLine();
+//    }
+//}
+
+Char[,] mas = new char[5, 2];
+mas[0, 0] = 'a';
+mas[1, 0] = 'b';
+mas[2, 0] = 'c';
+mas[3, 0] = 'k';
+mas[4, 0] = 'u';
+mas[0, 1] = 'd';
+mas[1, 1] = 'e';
+mas[2, 1] = 'f';
+mas[3, 1] = 't';
+mas[4, 1] = 'n';
+for (int i = 0; i <mas.GetUpperBound(0)+1; i++)
+{
+    for (int j = 0; j <mas.GetUpperBound(1)+1; j++)
+    {
+        Console.Write(mas[i, j] + " ");
+
+    }
+    Console.WriteLine();
+}
+Console.Write($"{mas[1, 0]}{mas[0, 0]}{mas[2, 0]}{mas[3, 0]}");
+
+
+
+
+
+
