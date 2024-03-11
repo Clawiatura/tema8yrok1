9d56346403439f0b5e4d8039853876bc0d7a3bbc
Clawiatura <maksimov19992805@icloud.com>
2024-03-11 11:36:40 +0200

    lampochka

--- Dvumernuemassivu/Program.cs
+++ Dvumernuemassivu/Program.cs
@@ -37,27 +37,27 @@
 //    }
 //}
 
-Char[,] mas = new char[5, 2];
-mas[0, 0] = 'a';
-mas[1, 0] = 'b';
-mas[2, 0] = 'c';
-mas[3, 0] = 'k';
-mas[4, 0] = 'u';
-mas[0, 1] = 'd';
-mas[1, 1] = 'e';
-mas[2, 1] = 'f';
-mas[3, 1] = 't';
-mas[4, 1] = 'n';
-for (int i = 0; i <mas.GetUpperBound(0)+1; i++)
-{
-    for (int j = 0; j <mas.GetUpperBound(1)+1; j++)
-    {
-        Console.Write(mas[i, j] + " ");
+//Char[,] mas = new char[5, 2];
+//mas[0, 0] = 'a';
+//mas[1, 0] = 'b';
+//mas[2, 0] = 'c';
+//mas[3, 0] = 'k';
+//mas[4, 0] = 'u';
+//mas[0, 1] = 'd';
+//mas[1, 1] = 'e';
+//mas[2, 1] = 'f';
+//mas[3, 1] = 't';
+//mas[4, 1] = 'n';
+//for (int i = 0; i <mas.GetUpperBound(0)+1; i++)
+//{
+//    for (int j = 0; j <mas.GetUpperBound(1)+1; j++)
+//    {
+//        Console.Write(mas[i, j] + " ");
 
-    }
-    Console.WriteLine();
-}
-Console.Write($"{mas[1, 0]}{mas[0, 0]}{mas[2, 0]}{mas[3, 0]}");
+//    }
+//    Console.WriteLine();
+//}
+//Console.Write($"{mas[1, 0]}{mas[0, 0]}{mas[2, 0]}{mas[3, 0]}");
 
 
 
