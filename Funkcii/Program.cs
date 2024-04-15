eefbd4391af46fc1ace52ab170f2f60baaae0a14
Clawiatura <maksimov19992805@icloud.com>
2024-04-15 11:43:51 +0200

    1213

--- Funkcii/Program.cs
+++ Funkcii/Program.cs
@@ -3,18 +3,25 @@
 //1.Суммирующую массивы и возвращающую в качестве результата
 //новый массив суммы.
 //2. Поиска максимального значения массива.
-using System;
+
+
+
+
+
+
+
 Random random = new Random();
+
 int[,] mas1 = new int[4, 4];
-for (int i= 0;i < mas1.GetLength(0);i++)
+for (int i = 0; i < mas1.GetLength(0); i++)
 {
-    mas1[i,0] = random.Next(-50, 50);
-    for (int j= 0;j < mas1.GetLength(1);j++)
-    {
-        mas1[i,j] = random.Next(-50, 50);
-        Console.Write(mas1[i, j]+" ");
-    }
-    Console.WriteLine();
+mas1[i, 0] = random.Next(-50, 50);
+for (int j = 0; j < mas1.GetLength(1); j++)
+{
+mas1[i, j] = random.Next(-50, 50);
+Console.Write(mas1[i, j] + " ");
+}
+Console.WriteLine();
 }
 Console.WriteLine("\n");
 int[,] mas2 = new int[4, 4];
@@ -24,33 +31,79 @@ for (int i = 0; i < mas2.GetLength(0); i++)
     for (int j = 0; j < mas2.GetLength(1); j++)
     {
         mas2[i, j] = random.Next(-50, 50);
-        Console.Write(mas2[i,j]+" ");
+        Console.Write(mas2[i, j] + " ");
     }
     Console.WriteLine();
 }
-int[,] mas = new int[4, 4];
-
-void sum(int[,] mas, int[,] mas1)
-
-{
-  for (int i = 0; i < mas.GetLength(0);i++)
+    int[,] sum(int[,] mas, int[,] mas1)
     {
-        for (int j = 0; j < mas.GetLength(1); j++)
+        int rows = mas.GetLength(0);
+        int cols = mas1.GetLength(1);
+        int[,] result = new int[4, 4];
+        for (int i = 0; i < rows; i++)
         {
-          mas[i, j] = mas1[i, j] + mas2[i, j];
-            Console.Write(mas[i,j]+" ");
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = mas[i, j] + mas1[i, j];
+
+            }
+
         }
-        Console.WriteLine();
+        return result;
 
     }
-    
+Console.WriteLine("\n");
+int[,] result = sum(mas1, mas2);
+for (int r = 0; r < result.GetLength(0); r++)
+{
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        Console.Write(result[r, j] + " ");
     }
-string[] GetTypeAndMax(int[,] mas)
-{ 
+    Console.WriteLine();
 }
-  
+
+for (int r = 0; r < result.GetLength(0); r++)
+{
+    
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        
+        int res = MaxNumber(result[r,j]);
+        Console.Write(res); break;
+        // Не могу понять, как сделать так, чтобы он просто нашел одно наибольшее число и вывел его
+
+    }
+    Console.WriteLine();
+}
+
+int MaxNumber(int x)
+
+    {
+        int maxvalue = 0;
+        int[,] result = sum(mas1, mas2);
+
+    foreach (int a in result)
+        {
+            maxvalue = maxvalue< a ? a : maxvalue;
+        }
+    return maxvalue;
+    }
 
 
 
 
-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
